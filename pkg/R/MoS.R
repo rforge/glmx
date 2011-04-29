5b92d4acbdf3d487c2e839d727663df4a2e56ce3
rkoenker <rkoenker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-29 09:07:35 +0000

    added MoS.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmx@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/MoS.R
@@ -0,0 +1,45 @@
+iwls <- function(object, maxit = 10) {
+
+  ## extract data
+  Y <- object$y ## quick&dirty  
+  X <- model.matrix(object, model = "mean")
+  Z <- model.matrix(object, model = "scale")
+  offset <- if(is.null(object$offset)) 0 else object$offset
+  dev.resids <- object$family$dev.resids
+  wts <- weights(object)
+  if(is.null(wts)) wts <- 1
+
+  ## pretend to be in starting values
+  par <- object$start
+  
+  for(i in 1:maxit) {
+    ## split up parameters
+    beta <- par[1:NCOL(X)]
+    gamma <- par[-(1:NCOL(X))]  
+
+    ## process linear predictors, links, etc.
+    scale_eta <- object$link$scale$linkfun(1) + drop(Z %*% gamma)
+    scale <- object$link$scale$linkinv(scale_eta)
+    eta <- drop(X %*% beta + offset) / scale
+    fog <- object$link$mean$mu.eta(eta) / scale
+    mu <- object$link$mean$linkinv(eta)
+    varmu <- object$family$variance(mu)   
+ 
+    ## compute weights
+    gbeta <- sqrt(wts) * (1 / varmu) * fog
+    ggamma <- - gbeta * eta * object$link$scale$mu.eta(scale_eta)
+    Hbeta <- sqrt(wts) * (1 / sqrt(varmu)) * fog
+    Hgamma <- - Hbeta * eta * object$link$scale$mu.eta(scale_eta)
+
+    ## scaled cross products  
+    A <- crossprod(cbind(Hbeta * X, Hgamma * Z))
+    b <- crossprod(cbind(gbeta * X, ggamma * Z), sqrt(wts) * (Y - mu))
+    step <- solve(A,b)
+    ## should probably have a step halving loop here
+    par <- par + step
+    dev <- sum(dev.resids(Y,mu,wts))
+  }
+  
+  names(par) <- names(coef(object))  
+  return(par)  
+}
