9f872d60c54c922e61af0dc2b937fef15792f635
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-26 17:33:08 +0000

    option profile=FALSE now also supported
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmx@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glmx.R
+++ pkg/R/glmx.R
@@ -1,7 +1,7 @@
 glmx.fit <- function(x, y, weights = NULL, offset = NULL,
   family = negative.binomial, xlink = "log", xstart = 0,
   profile = TRUE, nuisance = FALSE,           ## FIXME: optionally: extra pars as nuisance pars
-  control = glm.control(), xcontrol = list()) ## FIXME: maybe merge control arguments?
+  control = glm.control(), xcontrol = list())
 {
   ## process link for extra parameters
   if(!inherits(xlink, "link-glm")) xlink <- make.link(xlink)  
@@ -23,29 +23,12 @@ glmx.fit <- function(x, y, weights = NULL, offset = NULL,
   kstar <- if(family_start$family %in% c("gaussian", "Gamma", "inverse.gaussian")) k + q + 1L else k + q ## FIXME?
   ## FIXME: indicate whether dispersion needs to be estimated or not
 
-  if(profile) {
-    ## objective function
-    profile_loglik <- function(par) {
-      aic <- glm.fit(x, y, weights = weights, offset = offset, control = control, family = family(xlink$linkinv(par)))$aic
-      (aic - kstar) / 2
-    }
-
-    ## optimize profile likelihood  
-    opt1 <- optim(par = xstart, fn = profile_loglik, method = "BFGS", hessian = FALSE, control = xcontrol)
-    if(opt1$convergence > 0L) warning("optimization of profile likelihood failed to converge")
-
-    ## extract optimal parameters
-    xpar <- opt1$par
-  } else {
-    ## choose starting value of extra parameters
-    opt1 <- NULL
-    xpar <- xstart
+  ## objective function
+  profile_loglik <- function(par) {
+    aic <- glm.fit(x, y, weights = weights, offset = offset, control = control, family = family(xlink$linkinv(par)))$aic
+    (aic - kstar) / 2
   }
-  
-  ## optimal coefficients at chosen extra parameters
-  cf <- glm.fit(x, y, weights = weights, offset = offset, control = control, family = family(xlink$linkinv(xpar)))$coefficients
 
-  ## objective function and gradient
   full_loglik <- function(par) {
     beta <- par[1:k]
     gamma <- par[-(1:k)]
@@ -55,10 +38,27 @@ glmx.fit <- function(x, y, weights = NULL, offset = NULL,
     (f$aic(y, n, mu, weights, dev) - kstar) / 2
   }
 
+  if(profile) {
+    ## optimize profile likelihood first
+    opt1 <- optim(par = xstart, fn = profile_loglik, method = "BFGS", hessian = FALSE, control = xcontrol)
+    if(opt1$convergence > 0L) warning("optimization of profile likelihood failed to converge")
+
+    ## extract optimal parameters
+    xpar <- opt1$par
+
+    ## optimal coefficients at chosen extra parameters
+    cf <- glm.fit(x, y, weights = weights, offset = offset, control = control, family = family(xlink$linkinv(xpar)))$coefficients
+  } else {
+    ## use starting values of extra parameters
+    opt1 <- NULL
+    xpar <- xstart
+    cf <- glm.fit(x, y, weights = weights, offset = offset, control = control, family = family(xlink$linkinv(xpar)))$coefficients
+  }
+  
   ## optimize full likelihood
   opt2 <- optim(par = c(cf, xpar), fn = full_loglik, method = "BFGS", hessian = TRUE, control = xcontrol)
   if(opt2$convergence > 0L) warning("optimization of full likelihood failed to converge")
-
+    
   ## extract fitted values/parameters
   vc <- solve(as.matrix(opt2$hessian))
   beta <- as.vector(opt2$par[1:k])
