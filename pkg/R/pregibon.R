3a5d7cf963dae1307af9c00581ef8653dee81001
azeileis <azeileis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-30 12:06:27 +0000

    added lower.tail and log/log.p arguments -> still need checking
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glmx@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/pregibon.R
+++ pkg/R/pregibon.R
@@ -4,23 +4,29 @@
 .ab_to_lambda <- function(a, b)
   c(0, 1, a - b, a + b)
 
-qpregibon <- function(p, a = 0, b = 0)
+qpregibon <- function(p, a = 0, b = 0, lower.tail = TRUE, log.p = FALSE)
 {
   stopifnot(require("gld"))
+  if(log.p) p <- exp(p)
+  if(!lower.tail) p <- 1 - p
   qgl(p, .ab_to_lambda(a, b))/.ab_to_scale(a, b)
 }
 
-ppregibon <- function(q, a = 0, b = 0, tol = 1e-12)
+ppregibon <- function(q, a = 0, b = 0, lower.tail = TRUE, log.p = FALSE, tol = 1e-12)
 {
   stopifnot(require("gld"))
-  pgl(q * .ab_to_scale(a, b), .ab_to_lambda(a, b), inverse.eps = tol)
+  p <- pgl(q * .ab_to_scale(a, b), .ab_to_lambda(a, b), inverse.eps = tol)
+  if(!lower.tail) p <- 1 - p
+  if(log.p) p <- log(p)
+  return(p)
 }
 
-dpregibon <- function(x, a = 0, b = 0, tol = 1e-12)
+dpregibon <- function(x, a = 0, b = 0, log = FALSE, tol = 1e-12)
 {
   stopifnot(require("gld"))
   s <- .ab_to_scale(a, b)
-  dgl(x * s, .ab_to_lambda(a, b), inverse.eps = tol) * s
+  d <- dgl(x * s, .ab_to_lambda(a, b), inverse.eps = tol) * s
+  if(log) log(d) else d
 }
 
 rpregibon <- function(n, a = 0, b = 0)
